8c8b05eea82888e52d94131dbe572f003ad90c54
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2020-12-27 14:27:11 +0200

    fix edit profile page

--- edit-profile.php
+++ edit-profile.php
@@ -1,8 +1,12 @@
 <?php include("layouts/header.php") ?>
 
+  <div class="intro-y flex items-center mt-1 h-10">
+    <h2 class="text-lg font-medium text-gray-600 truncate mr-5">Edit Profile</h2>
+  </div>
+
 	<!-- BEGIN: Profile Info -->
 
-	<form id="profile" class="grid grid-cols-12 gap-6 mt-5">
+	<form id="profile" class="grid grid-cols-12 gap-6">
 
 		<div class="intro-y box px-5 pt-5 mt-5 col-span-12 sm:col-span-12 lg:col-span-4">
 			<div class="flex flex-col lg:flex-row pb-5 -mx-5">
@@ -29,30 +33,38 @@
 
 
 			<div class="grid grid-cols-12 gap-6">
-				<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+				<div class=" col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row px-5 py-1 -mx-5">
+          <label class="text-gray-600 mb-3 text-lg">Name</label>
 					<input type="text" class="input w-full border mt-2" placeholder="Name"> 
 				</div>
 
-				<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+				<div class=" col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row px-5 py-1 -mx-5">
+          <label class="text-gray-600 mb-3 text-lg">Phone Number</label>
 					<input type="text" class="input w-full border mt-2" placeholder="Phone Number"> 
 				</div>
 
-				<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+				<div class=" col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row px-5 py-1 -mx-5">
+          <label class="text-gray-600 mb-3 text-lg">Address</label>
 					<input type="text" class="input w-full border mt-2" placeholder="Address"> 
 				</div>
 
-				<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+				<div class=" col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row px-5 py-1 -mx-5">
+          <label class="text-gray-600 mb-3 text-lg">Email</label>
 					<input type="email" class="input w-full border mt-2" placeholder="Email"> 
+        </div>
+        
+        <div class=" col-span-12 lg:flex-row px-5 pb-5 -mx-5">
+          <label class="text-gray-600 mb-3 text-lg">Role</label>
+          <select class="chronic-diseases-selector tail-select w-full">
+            <option> Select The Role </option>
+            <option value="0">Doctor</option>
+            <option value="1">Receptionist</option>
+          </select>
 				</div>
 
 			</div>
 
 			</div>
-
-				<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-12 lg:flex-row p-5 -mx-5">
-					<textarea class="editor w-full" name="editor"> 
-					</textarea> 
-				</div>
 
 				<button type="submit" class="button w-32 mr-2 mb-2 flex items-center justify-center bg-theme-1 text-white"> 
 					<i data-feather="save" class="w-4 h-4 mr-2  ml-2"></i> Save 
