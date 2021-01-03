291b32e8603e4df7f3e3e2c7604d63031f560cbd
mahmoudsiliman <mahmoudsiliman75@gmail.com>
2021-01-03 14:51:25 +0200

    edit patients report

--- patients-reports.php
+++ patients-reports.php
@@ -9,7 +9,7 @@
   <div class="intro-y box pb-3" style="z-index:1">
     <div class="flex sm:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
     
-      <form class="flex mr-auto grid grid-cols-12 gap-6" id="tabulator-html-filter-form">
+      <form class="print flex mr-auto grid grid-cols-12 gap-6" id="tabulator-html-filter-form">
 
         <div class="sm:flex-row items-center col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-4 mt-2">
           <label class="text-gray-600 mb-3 text-lg">Sarch For</label>
@@ -23,12 +23,8 @@
         </div>
 
       </form>
-
-      <button type="submit" class="button w-40 mr-5 ml-5 flex items-center justify-center bg-theme-1 text-white"> 
-        <i data-feather="plus-square" class="w-4 h-4 mr-2  ml-2"></i>
-        <a href="reservation.php">
-          <span> Add New Patient <span> 
-        </a>
+      <button type="submit" class="print print-tables button translate-y-3 mt-2 ml-2 flex items-center justify-center bg-theme-1 text-white"> 
+        <i data-feather="save" class="w-4 h-4 mr-2  ml-2"></i> Print 
       </button>
     </div>
 
@@ -38,17 +34,20 @@
           <tr class="bg-gray-200 dark:bg-dark-1"> 
             <th class="border-b-2 dark:border-dark-5 whitespace-no-wrap">#</th> 
             <th class="border-b-2 dark:border-dark-5 whitespace-no-wrap">Patient Name</th> 
-            <th class="border-b-2 dark:border-dark-5 whitespace-no-wrap">Actions</th> 
+            <th class="border-b-2 dark:border-dark-5 whitespace-no-wrap">Phone Number</th> 
+            <th class="border-b-2 dark:border-dark-5 whitespace-no-wrap">Visites</th> 
+            <th class="print border-b-2 dark:border-dark-5 whitespace-no-wrap">Actions</th> 
           </tr> 
         </thead> 
 
         <tbody> 
           <tr> 
             <td class="border-b dark:border-dark-5">1</td> 
-
             <td class="border-b dark:border-dark-5">Angelina</td> 
+            <td class="border-b dark:border-dark-5">01000000001</td> 
+            <td class="border-b dark:border-dark-5">4</td> 
 
-            <td class="border-b dark:border-dark-5 whitespace-no-wrap">
+            <td class="print border-b dark:border-dark-5 whitespace-no-wrap">
 
               <button class="button px-2 mr-1 mb-2 bg-theme-9 text-white tooltip" title="Financial Report"> 
                 <a href="patient-financ-report.php" class="w-5 h-5 flex items-center justify-center"> 
@@ -56,27 +55,9 @@
                 </a> 
               </button>
 
-              <button class="button px-2 mr-1 mb-2 bg-theme-12 text-white tooltip" title="Patient History With Visits" > 
+              <button class="button px-2 mr-1 mb-2 bg-theme-12 text-white tooltip" title="Patient History" > 
                 <a href="patient-history-with-visites.php" class="w-5 h-5 flex items-center justify-center"> 
                   <i data-feather="hard-drive" class="w-4 h-4"></i> 
-                </a> 
-              </button>
-
-              <button class="button px-2 mr-1 mb-2 bg-theme-1 text-white tooltip" title="Add Reservation"> 
-                <a href="javascript:;" data-toggle="modal" data-target="#add-reservation" class="w-5 h-5 flex items-center justify-center inline-block bg-theme-1 text-white">
-                  <i data-feather="plus" class="w-4 h-4"></i> 
-                </a> 
-              </button>
-
-              <button class="button px-2 mr-1 mb-2 bg-theme-9 text-white tooltip" title="Edit"> 
-                <a href="#" class="w-5 h-5 flex items-center justify-center"> 
-                  <i data-feather="edit" class="w-4 h-4"></i> 
-                </a> 
-              </button>
-
-              <button class="button px-2 mr-1 mb-2 bg-theme-6 text-white tooltip" title="Delete"> 
-                <a href="#" class="w-5 h-5 flex items-center justify-center"> 
-                  <i data-feather="trash" class="w-4 h-4"></i> 
                 </a> 
               </button>
 
@@ -86,10 +67,11 @@
 
           <tr> 
             <td class="border-b dark:border-dark-5">2</td> 
-
             <td class="border-b dark:border-dark-5">Name 2</td> 
+            <td class="border-b dark:border-dark-5">01000000002</td> 
+            <td class="border-b dark:border-dark-5">3</td> 
 
-            <td class="border-b dark:border-dark-5 whitespace-no-wrap">
+            <td class="print border-b dark:border-dark-5 whitespace-no-wrap">
 
               <button class="button px-2 mr-1 mb-2 bg-theme-9 text-white tooltip" title="Financial Report"> 
                 <a href="patient-financial-report-cash.php" class="w-5 h-5 flex items-center justify-center"> 
@@ -97,27 +79,9 @@
                 </a> 
               </button>
 
-              <button class="button px-2 mr-1 mb-2 bg-theme-12 text-white tooltip" title="Patient History With Visits" > 
+              <button class="button px-2 mr-1 mb-2 bg-theme-12 text-white tooltip" title="Patient History ">            
                 <a href="patient-history-with-visites.php" class="w-5 h-5 flex items-center justify-center"> 
                   <i data-feather="hard-drive" class="w-4 h-4"></i> 
-                </a> 
-              </button>
-
-              <button class="button px-2 mr-1 mb-2 bg-theme-1 text-white tooltip" title="Add Reservation"> 
-                <a href="javascript:;" data-toggle="modal" data-target="#add-reservation" class="w-5 h-5 flex items-center justify-center inline-block bg-theme-1 text-white">
-                  <i data-feather="plus" class="w-4 h-4"></i> 
-                </a> 
-              </button>
-
-              <button class="button px-2 mr-1 mb-2 bg-theme-9 text-white tooltip" title="Edit"> 
-                <a href="#" class="w-5 h-5 flex items-center justify-center"> 
-                  <i data-feather="edit" class="w-4 h-4"></i> 
-                </a> 
-              </button>
-
-              <button class="button px-2 mr-1 mb-2 bg-theme-6 text-white tooltip" title="Delete"> 
-                <a href="#" class="w-5 h-5 flex items-center justify-center"> 
-                  <i data-feather="trash" class="w-4 h-4"></i> 
                 </a> 
               </button>
 
@@ -127,24 +91,6 @@
 
         </tbody> 
       </table> 
-
-      <!-- START:: PAGINATION -->
-      <div class="intro-y flex flex-wrap sm:flex-row sm:flex-no-wrap items-center mt-3">
-        <ul class="pagination">
-          <li>
-            <a class="pagination__link" href=""> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left w-4 h-4"><polyline points="15 18 9 12 15 6"></polyline></svg> </a>
-          </li>
-
-          <li> <a class="pagination__link" href="">1</a> </li>
-          <li> <a class="pagination__link pagination__link--active" href="">2</a> </li>
-          <li> <a class="pagination__link" href="">3</a> </li>
-
-          <li>
-            <a class="pagination__link" href=""> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right w-4 h-4"><polyline points="9 18 15 12 9 6"></polyline></svg> </a>
-          </li>
-        </ul>
-      </div>
-      <!-- START:: PAGINATION -->
 
       <!-- START:: ADD RESERVATION MODAL -->
       <div class="modal" id="add-reservation">
